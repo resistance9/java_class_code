a547a379b9f40067318cc6b7adf582db63d493ce
resistance9 <lwh0095@gmail.com>
2026-03-12 21:15:29 +0900

    START

---
+++ inclass_code/2주차/Best.java
@@ -0,0 +1,6 @@
+public class Best {
+    public static void main(String[] args) {
+        System.out.println("학기 시작되었어요!");
+        System.out.println("이번 학기 자바를 열심히 공부해서 최고의 Sotfware 개발자가 될 거예요~~");
+    }
+}
