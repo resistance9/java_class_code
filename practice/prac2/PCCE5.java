c3b84d714a068821d17246b1123940774c9430c9
resistance9 <lwh0095@gmail.com>
2026-06-04 10:50:40 +0900

    14주차 까지 완료

--- practice/prac2/PCCE5.java
+++ practice/prac2/PCCE5.java
@@ -7,7 +7,7 @@ class PCCE5 {
         for(int i=0; i<cpr.length; i++){
             for(int j=0; j<basic_order.length; j++){
                 if(cpr[i].equals(basic_order[j])){
-                    answer[i] = j + 1;   // 빈칸: j+1 (1-indexed 순서)
+                    answer[i] = j + 1;
                     break;
                 }
             }
--- practice/prac2/PCCE6.java
+++ practice/prac2/PCCE6.java
@@ -4,7 +4,7 @@ class PCCE6 {
     public int solution(int storage, int usage, int[] change) {
         int total_usage = 0;
         for(int i=0; i<change.length; i++){
-            usage += usage * change[i] / 100;  // ← 수정: usage 누적 변화
+            usage += usage * change[i] / 100;
             total_usage += usage;
             if(total_usage > storage){
                 return i;
